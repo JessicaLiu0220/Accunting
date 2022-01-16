dbbd03edc266c5eda3e17a15e5ceadb35cdfb036
Jessica <1330784336@qq.com>
2022-01-16 14:48:15 +0800

    完成基本numberpad组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,25 +1,63 @@
 <template>
   <div class="numberPad">
-    <div class="output">0</div>
-    <div class="buttons clearfix">
-      <button @click="outputNumber">1</button>
-      <button @click="outputNumber">2</button>
-      <button @click="outputNumber">3</button>
+    <div class="output">{{ output }}</div>
+    <div class="buttons">
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
       <button @click="remove"><Icon name="remove" /></button>
-      <button @click="outputNumber">4</button>
-      <button @click="outputNumber">5</button>
-      <button @click="outputNumber">6</button>
-      <button @click="clear">清除</button>
-      <button @click="outputNumber">7</button>
-      <button @click="outputNumber">8</button>
-      <button @click="outputNumber">9</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
       <button @click="ok" class="ok">OK</button>
-      <button @click="outputNumber">.</button>
-      <button @click="outputNumber" class="zero">0</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+@Component
+export default class NumberPad extends Vue {
+  output = "0";
+  inputContent(event: MouseEvent) {
+    const button = event.target as HTMLButtonElement;
+    const input = button.textContent!;
+    if (this.output.length === 16) {
+      return;
+    }
+    if (this.output === "0") {
+      if ("0123456789".indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf(".") >= 0 && input === ".") {
+      return;
+    }
+    this.output += input;
+  }
+  remove() {
+    if (this.output.length === 1) {
+      this.output = "0";
+    } else {
+      this.output = this.output.slice(0, -1);
+    }
+  }
+  clear() {
+    this.output = "0";
+  }
+  // ok() {}
+}
+</script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
